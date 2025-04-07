86e45b0e5751070aa86372e0385c72706b402a7c
HYEONGYUN0823 <shiningin00@naver.com>
2025-04-07 12:39:01 +0900

    first

---
+++ freshly/src/main/java/com/a7a7/FreshlyApplication.java
@@ -0,0 +1,13 @@
+package com.a7a7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshlyApplication.class, args);
+	}
+
+}
