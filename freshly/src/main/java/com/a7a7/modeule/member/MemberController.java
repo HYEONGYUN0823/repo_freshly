4140e8cd017b197579e5454d7f361b851eb5e27f
HYEONGYUN0823 <shiningin00@naver.com>
2025-04-08 12:23:16 +0900

    khy

--- freshly/src/main/java/com/a7a7/modeule/member/MemberController.java
+++ freshly/src/main/java/com/a7a7/modeule/member/MemberController.java
@@ -2,9 +2,20 @@ package com.a7a7.modeule.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class MemberController {
 	@Autowired
 	MemberService signService;
+	
+	@RequestMapping(value = "/web/login")
+	public String webLogin() {
+		
+		return "web/sign/login";
+	}
+	
 }
