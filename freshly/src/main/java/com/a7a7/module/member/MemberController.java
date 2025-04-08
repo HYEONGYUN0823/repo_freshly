7bed917727c7fd496c1f35eb62eae22d90ae49aa
tdj4909 <tdj4909@gmail.com>
2025-04-08 14:55:07 +0900

    ojw

--- freshly/src/main/java/com/a7a7/module/member/MemberController.java
+++ freshly/src/main/java/com/a7a7/module/member/MemberController.java
@@ -3,6 +3,7 @@ package com.a7a7.module.member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -10,12 +11,22 @@ public class MemberController {
 	@Autowired
 	MemberService service;
 	
+	// 공통
+	@ResponseBody
+	@RequestMapping(value = "/loginProc")
+	public String loginProc() {
+		
+		return "web/sign/login";
+	}
+	
+	// Web
 	@RequestMapping(value = "/web/login")
 	public String webLogin() {
 		
 		return "web/sign/login";
 	}
 	
+	// Mobile
 	@RequestMapping(value = "/mob/login")
 	public String mobLogin() {
 		
