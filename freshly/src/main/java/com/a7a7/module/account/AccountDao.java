be1c8f95d3a0d2386f9824914fa41c753809475e
tdj4909 <tdj4909@gmail.com>
2025-04-09 17:19:53 +0900

    ojw

--- freshly/src/main/java/com/a7a7/module/account/AccountDao.java
+++
@@ -1,8 +0,0 @@
-package com.a7a7.module.account;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountDao {
-
-}
--- freshly/src/main/java/com/a7a7/module/account/AccountService.java
+++
@@ -1,8 +0,0 @@
-package com.a7a7.module.account;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class AccountService {
-
-}
