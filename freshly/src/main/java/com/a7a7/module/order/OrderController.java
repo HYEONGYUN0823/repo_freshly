247afbdfe08d5b3bfbc76caffee8dcf422547a01
HYEONGYUN0823 <shiningin00@naver.com>
2025-04-10 14:44:28 +0900

    khy

--- freshly/src/main/java/com/a7a7/module/order/OrderController.java
+++ freshly/src/main/java/com/a7a7/module/order/OrderController.java
@@ -7,21 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OrderController {
-
 	@Autowired
 	OrderService service;
 	
 	// 모바일 주문 리스트 화면 입니다.
 	@RequestMapping(value ="/mob/account/orderlist")
 	public String moborderlist(Model model) {
-		model.addAttribute("list",service.selectOrderList());
+		model.addAttribute("list", service.selectOrderList());
 		return "/mobile/account/orderlist";
 	}
 	
 	// 앱 주문 리스트 화면 입니다.
 	@RequestMapping(value ="/web/account/orderList")
 	public String weborderlist(Model model) {
-		model.addAttribute("list",service.selectOrderList());
+		model.addAttribute("list", service.selectOrderList());
 		return "/web/account/orderList";
 	}
 	
