8e8de2d5bb55ac22da4d29b632607d0d2dd953a4
YONGYONGB <aa39421371@gmail.com>
2025-04-16 10:09:09 +0900

    kyy

--- freshly/src/main/java/com/a7a7/module/basic/BasicController.java
+++ freshly/src/main/java/com/a7a7/module/basic/BasicController.java
@@ -155,43 +155,4 @@ public class BasicController {
 	
 	
 	
-	
-	
-	
-	
-	
-//	**********************************************
-//	Mobile
-//	**********************************************
-	// 식료품 관리
-	// 식료품 list 화면
-	@RequestMapping(value = "/mob/basic/groceryList")
-	public String selectMobGroceryList(Model model) {
-		
-//		model.addAttribute("list", service.selectGroceryList(dto));
-		
-		return "mobile/basic/groceryList";
-	}
-	
-	// 거래처 관리
-	// 거래처 list 화면
-	@RequestMapping(value = "/mob/basic/accountList")
-	public String selectMobAccountList(Model model) {
-		
-		model.addAttribute("list", service.selectAccountList());
-		
-		return "mobile/basic/accountList";
-	}
-	
-	// 공장 관리
-	// 공장 list 화면
-	@RequestMapping(value = "/mob/basic/factoryList")
-	public String selectMobFactoryList(Model model) {
-		
-		model.addAttribute("list", service.selectFactoryList());
-		
-		return "mobile/basic/factoryList";
-	}
-	
-	
 }
